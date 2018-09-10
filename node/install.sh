8acdf80633976a3f0e64de266551b1bb5698d1ce
Brandon Doran <bdoran@gmail.com>
2018-09-09 21:43:15 -0700

    Install nvm and node lts

--- node/install.sh
+++ node/install.sh
@@ -1,4 +1,9 @@
-if test ! $(which spoof)
-then
-  sudo npm install spoof -g
-fi
+# Install nvm
+curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.11/install.sh | bash
+
+# Install latest node LTS release
+nvm install --lts
+
+# Set the default Node version, to be used in any new shell.
+# Use the node LTS version we just installed.
+nvm alias default $(node -v)
