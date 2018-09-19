0154e2a2b93cfd1b3a34c17d9d724b0012050211
Brandon Doran <bdoran@gmail.com>
2018-09-18 21:11:25 -0700

    Update tmux setup

--- tmux/install.sh
+++ tmux/install.sh
@@ -3,7 +3,3 @@
 # Install tmuxinator: https://github.com/tmuxinator/tmuxinator
 echo "› install Tmuxinator"
 gem install tmuxinator
-
-# Download completions
-mkdir -p ~/.bin
-curl https://raw.githubusercontent.com/tmuxinator/tmuxinator/master/completion/tmuxinator.zsh --output ~/.bin/tmuxinator.zsh
