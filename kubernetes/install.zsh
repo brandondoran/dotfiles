c00d0786061b02fc1be8ae97d42ef5b20c6d6181
Brandon Doran <bdoran@gmail.com>
2020-04-12 15:37:04 -0700

    Add installation for kcrew (kubectl plugin manager)

---
+++ kubernetes/install.zsh
@@ -0,0 +1,10 @@
+# Install krew, a plugin manager for kubectl
+# https://krew.sigs.k8s.io/
+(
+  set -x; cd "$(mktemp -d)" &&
+  curl -fsSLO "https://github.com/kubernetes-sigs/krew/releases/latest/download/krew.{tar.gz,yaml}" &&
+  tar zxvf krew.tar.gz &&
+  KREW=./krew-"$(uname | tr '[:upper:]' '[:lower:]')_amd64" &&
+  "$KREW" install --manifest=krew.yaml --archive=krew.tar.gz &&
+  "$KREW" update
+)
--- kubernetes/path.zsh
+++ kubernetes/path.zsh
@@ -3,3 +3,6 @@ export KUBECONFIG=$HOME/.kube/config
 
 # Editor for kubectl (Kubernetes cli)
 export KUBE_EDITOR="vim"
+
+# Add krew to path
+export PATH="${KREW_ROOT:-$HOME/.krew}/bin:$PATH"
